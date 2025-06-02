75c06d32b6fc60337898df5d7f9eaa7c58525334
jaka8m <paoandest82@gmail.com>
2025-06-02 12:13:56 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,13 +1,19 @@
 import { TelegramWildcardBot } from './bot.js';
 
-const TELEGRAM_BOT_TOKEN = TWIG_ENV.TELEGRAM_BOT_TOKEN || '';
-const OWNER_ID = Number(TWIG_ENV.OWNER_ID || 0);
-
-const bot = new TelegramWildcardBot(TELEGRAM_BOT_TOKEN, undefined, OWNER_ID);
+let bot;
 
 export default {
-  async fetch(request) {
-    if (request.method !== 'POST') return new Response('Method Not Allowed', { status: 405 });
+  async fetch(request, env) {
+    // Inisialisasi bot hanya sekali saja
+    if (!bot) {
+      const TELEGRAM_BOT_TOKEN = env.TELEGRAM_BOT_TOKEN || '';
+      const OWNER_ID = Number(env.OWNER_ID || 0);
+      bot = new TelegramWildcardBot(TELEGRAM_BOT_TOKEN, undefined, OWNER_ID);
+    }
+
+    if (request.method !== 'POST') {
+      return new Response('Method Not Allowed', { status: 405 });
+    }
 
     try {
       const update = await request.json();
